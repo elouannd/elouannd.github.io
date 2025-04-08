0b06ed73b0c87aef546b370ddd74c5e34186b3b4
Elouann <elouann.dom@gmail.com>
2025-04-08 18:46:43 -0400

    v1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // We'll import the styles for the gradient
+
+function App() {
+  return (
+    <div className="App gradient-background">
+      <main>
+        <h1>Elouann</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
